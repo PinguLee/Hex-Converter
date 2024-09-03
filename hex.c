a9d536e7e2db3a5bbe85a61f6ff803cbcefef8fc
pingulee <pingulee@soosan.co.kr>
2024-09-03 17:51:26 +0900

    명령어 분기 처리

--- hex.c
+++ hex.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-    char buffer[256];
-    printf("fgets 입력 : ");
-    fgets(buffer, sizeof(buffer), stdin);
-    printf("%s\n", buffer);
+    char string_data[256];
+
+    while (1) {
+        printf("> ");
+        fgets(string_data, sizeof(string_data), stdin);
+
+        string_data[strcspn(string_data, "\n")] = '\0';
+
+        if (strcmp(string_data, "/input") == 0) {
+            printf("%s\n", string_data);
+        } else if (strcmp(string_data, "/help") == 0) {
+            printf("사용 가능한 명령어\n");
+            printf("/input : 문자열 입력\n");
+            printf("/exit : 프로그램 종료\n");
+        } else if (strcmp(string_data, "/exit") == 0) {
+            break;
+        } else {
+            printf("다시 입력하세요. 도움말 : /help");
+        }
+    }
+
     return 0;
 }
