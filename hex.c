45d4a6142034bbedc50d261e3a9e8f02bb36e7bb
pingulee <pingulee@soosan.co.kr>
2024-09-03 17:57:50 +0900

    문자열 입력 받기

--- hex.c
+++ hex.c
@@ -3,24 +3,27 @@
 #include <string.h>
 
 int main(void) {
+    char command[5];
     char string_data[256];
 
     while (1) {
         printf("> ");
-        fgets(string_data, sizeof(string_data), stdin);
+        fgets(command, sizeof(command), stdin);
 
-        string_data[strcspn(string_data, "\n")] = '\0';
+        command[strcspn(command, "\n")] = '\0';
 
-        if (strcmp(string_data, "/input") == 0) {
-            printf("%s\n", string_data);
-        } else if (strcmp(string_data, "/help") == 0) {
-            printf("사용 가능한 명령어\n");
-            printf("/input : 문자열 입력\n");
-            printf("/exit : 프로그램 종료\n");
-        } else if (strcmp(string_data, "/exit") == 0) {
+        if (strcmp(command, "/input") == 0) {
+            printf(" -> ");
+            fgets(string_data, sizeof(string_data), stdin);
+            printf("%s", string_data);
+        } else if (strcmp(command, "/help") == 0) {
+            printf("command\n");
+            printf("/input : input\n");
+            printf("/exit : exit\n");
+        } else if (strcmp(command, "/exit") == 0) {
             break;
         } else {
-            printf("다시 입력하세요. 도움말 : /help");
+            printf("help? : /help");
         }
     }
 
