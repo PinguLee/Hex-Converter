a28d61a0e33850647fff77ebf86f276fcf8a9983
pingulee <pingulee@soosan.co.kr>
2024-09-03 17:41:03 +0900

    gitignore

--- hex.c
+++ hex.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 
 int main(void) {
-    // SetConsoleOutputCP(65001);  // 출력 코드 페이지를 UTF-8로 설정
-    // SetConsoleCP(65001);  // 입력 코드 페이지를 UTF-8로 설정
-
     char buffer[256];
-    printf("입력 : ");
+    printf("fgets 입력 : ");
     fgets(buffer, sizeof(buffer), stdin);
-    printf("%s", buffer);
+    printf("%s\n", buffer);
     return 0;
 }
