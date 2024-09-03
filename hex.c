f8a21fe78bf32d962a431acc75bcc50201a75e05
pingulee <pigulee@soosan.co.kr>
2024-09-03 16:08:02 +0900

    입력 테스트

--- hex.c
+++ hex.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
-int main(void) { 
-    printf("test");
+int main(void) {
+    // SetConsoleOutputCP(65001);  // 출력 코드 페이지를 UTF-8로 설정
+    // SetConsoleCP(65001);  // 입력 코드 페이지를 UTF-8로 설정
+
+    char buffer[256];
+    printf("입력 : ");
+    fgets(buffer, sizeof(buffer), stdin);
+    printf("%s", buffer);
     return 0;
 }
